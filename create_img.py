08f869ae8a926e9d2587f3420275fcc31ab22433
Evin Dunn <evindunn14@gmail.com>
2019-09-23 10:22:20 -0700

    Python can mount & unmount loop device

--- create_img.py
+++ create_img.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import logging
+import os
 import subprocess as sp
 import sys
 
@@ -24,6 +25,9 @@ CMD_LOOP_DEV_FORMAT = """
     mkfs.vfat /dev/{part_1_name}
     mkfs.ext4 /dev/{part_2_name}
 """
+
+CMD_MNT = "mount {} {} {}"
+CMD_UMNT = "umount {}"
 
 LOG_FMT_MSG = "[%(asctime)s][%(levelname)s] %(message)s"
 LOG_FMT_DATE = "%H:%M:%S %Y-%m-%d"
@@ -97,6 +101,45 @@ def format_loop_dev(loop_dev):
     return True
 
 
+def mount_device(dev_path, mnt_path, opts=""):
+    """
+    Runs 'mount {opts} {dev_path} {mnt_path}'
+    :param dev_path: Path to block device to mount
+    :param mnt_path: Path to mount block device on
+    :param opts: Mount options
+    :return: Whether the operation was successful
+    """
+    completed_process = sp.run(
+        CMD_MNT.format(
+            opts,
+            dev_path,
+            mnt_path
+        ),
+        shell=True,
+        stdout=sp.DEVNULL,
+        stderr=sp.PIPE
+    )
+    if completed_process.returncode != 0:
+        logging.error(completed_process.stderr.decode("utf-8"))
+        return False
+    return True
+
+
+def unmount_device(dev_or_mount_path):
+    """
+    :param dev_or_mount_path: Filesystem or block device path to unmount
+    :return: Whether the operation was successful
+    """
+    completed_process = sp.run(
+        CMD_UMNT.format(dev_or_mount_path),
+        shell=True,
+        stdout=sp.DEVNULL,
+        stderr=sp.PIPE
+    )
+    if completed_process.returncode != 0:
+        logging.error(completed_process.stderr.decode("utf-8"))
+        return False
+    return True
 
 
 def delete_loop_dev(loop_dev):
@@ -131,6 +174,8 @@ def main():
         return 1
 
     loop_dev = img_file_to_loop_dev(img_file)
+    boot_partition = "{}p1".format(loop_dev)
+    root_partition = "{}p2".format(loop_dev)
     if loop_dev is None:
         return 1
 
@@ -142,7 +187,28 @@ def main():
     if not format_loop_dev(loop_dev):
         return 1
 
+    logging.info("Mounting {} on /mnt...".format(root_partition))
+    if not mount_device(root_partition, "/mnt", "-i -o exec,dev"):
+        return 1
+
+    try:
+        os.makedirs("/mnt/boot/firmware", mode=0o755)
+    except OSError as e:
+        logging.error(e)
+        return 1
+
+    logging.info("Mounting {} on /mnt/boot/firmware...".format(boot_partition))
+    if not mount_device(boot_partition, "/mnt", "-i -o exec,dev"):
+        return 1
+
     # Clean up
+    logging.info("Unmounting {}...".format(boot_partition))
+    if not unmount_device(boot_partition):
+        return 1
+    logging.info("Unmounting {}...".format(root_partition))
+    if not unmount_device(root_partition):
+        return 1
+
     logging.info("Deleting loop device {}...".format(loop_dev))
     if not delete_loop_dev(loop_dev):
         return 1
